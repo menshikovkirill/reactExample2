9cfe418758d44b224bc848e093f0f41776d92a39
Sanshain <my-email@ya.ru>
2020-08-13 22:02:56 +0300

    All files

---
+++ precache-manifest.93c04e7334390e88f2fd820d4f53fc3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a594ba34d5baa031d1811adf01f8008",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cc4068648f7d95e1f7f",
+    "url": "/static/js/2.e79dead0.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.e79dead0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbbf3cf2eb610d4fa08b",
+    "url": "/static/js/main.1b872b12.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
